aac5ecfd82e04385bdbb5a504d24733c20313547
Myskal <anarbekovamyskak6@gmail.com>
2024-03-24 13:35:50 +0600

    Dto

--- src/main/java/com/myskal/website/DTO/UserDTO.java
+++ src/main/java/com/myskal/website/DTO/UserDTO.java
@@ -1,15 +1,24 @@
 package com.myskal.website.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.apache.catalina.authenticator.SavedRequest;
+
+import java.util.Set;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
     private Long id;
     private String username;
+
+    private Set<PostDTO> posts;
+
 }

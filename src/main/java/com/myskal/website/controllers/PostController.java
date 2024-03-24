54390831ef9df4feed0041c5149fc2d2ce5d459e
Myskal <anarbekovamyskak6@gmail.com>
2024-03-24 22:13:08 +0600

    controllers

--- src/main/java/com/myskal/website/controllers/PostController.java
+++ src/main/java/com/myskal/website/controllers/PostController.java
@@ -1,8 +1,9 @@
 package com.myskal.website.controllers;
 
-
 import com.myskal.website.DTO.PostDTO;
 import com.myskal.website.services.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Api(tags = "Posts")
 @RequestMapping("/api/posts")
 public class PostController {
 
@@ -23,27 +25,32 @@ public class PostController {
         this.postService = postService;
     }
 
+    @ApiOperation(value = "Get a post by ID", notes = "Retrieve details of a post based on its ID")
     @GetMapping("/{id}")
     public PostDTO getPostById(@PathVariable Long id) {
         return postService.getPostById(id);
     }
 
+    @ApiOperation(value = "Get all posts", notes = "Retrieve details of all posts")
     @GetMapping
     public List<PostDTO> getAllPosts() {
         return postService.getAllPosts();
     }
 
+    @ApiOperation(value = "Create a new post", notes = "Create a new post with the provided details")
     @PostMapping("/posts")
     public ResponseEntity<PostDTO> createPost(@Validated @RequestBody PostDTO newPost) {
         PostDTO savedPost = postService.savePost(newPost);
         return ResponseEntity.created(URI.create("/api/posts/" + savedPost.getId())).body(savedPost);
     }
 
+    @ApiOperation(value = "Update a post", notes = "Update an existing post with the provided details")
     @PutMapping("/{id}")
     public PostDTO updatePost(@PathVariable Long id, @RequestBody PostDTO postDTO) {
         return postService.updatePost(id, postDTO);
     }
 
+    @ApiOperation(value = "Delete a post", notes = "Delete a post based on its ID")
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable Long id) {
         postService.deletePost(id);
--- src/main/java/com/myskal/website/controllers/UserController.java
+++ src/main/java/com/myskal/website/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.myskal.website.controllers;
 
 import com.myskal.website.DTO.UserDTO;
 import com.myskal.website.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Api(tags = "Users")
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -21,31 +24,34 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "Get a user by ID", notes = "Retrieve details of a user based on its ID")
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @ApiOperation(value = "Get all users", notes = "Retrieve details of all users")
     @GetMapping
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @ApiOperation(value = "Create a new user", notes = "Create a new user with the provided details")
     @PostMapping("/users")
     public ResponseEntity<UserDTO> createUser(@Validated @RequestBody UserDTO newUser) {
         UserDTO savedUser = userService.saveUser(newUser);
         return ResponseEntity.created(URI.create("/api/users/" + savedUser.getId())).body(savedUser);
     }
 
-
+    @ApiOperation(value = "Update a user", notes = "Update an existing user with the provided details")
     @PutMapping("/{id}")
     public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
         return userService.updateUser(id, userDTO);
     }
 
+    @ApiOperation(value = "Delete a user", notes = "Delete a user based on its ID")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
 }
-
